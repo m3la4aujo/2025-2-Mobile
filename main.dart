b1e4b1f8a687acb87d4f0196edc38aeb6e45c5cd
m3la4aujo <167586116+m3la4aujo@users.noreply.github.com>
2025-09-04 00:01:49 +0000

    feat: if statement

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main(){
+    int idade = 18;
+    if (idade >= 18)
+    {
+        print('Pode entrar na balada');        
+    }
+    else{
+        print('Nao pode entrar');
+    }
+    print ('fim do programa');
+}
+
