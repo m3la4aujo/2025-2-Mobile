e17d9c10f5d80fd4632889910100ca5778f04dd3
m3la4aujo <167586116+m3la4aujo@users.noreply.github.com>
2025-09-10 23:30:07 +0000

    feat: Exercicio_1

---
+++ exercicio_1.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+
+    void main(){
+        int idade = 18;
+        if (idade >= 16)
+        {
+            print('Você pode votar!!');
+
+        }
+        else {
+            print('Você ainda não tem idade para votar!');
+
+        }
+    }
