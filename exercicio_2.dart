0eb13ec243fa230cb46314cd431e244a08531b04
m3la4aujo <167586116+m3la4aujo@users.noreply.github.com>
2025-09-11 00:18:18 +0000

    feat: Exercicio_2

--- exercicio_2.dart
+++ exercicio_2.dart
@@ -2,9 +2,23 @@ import 'dart:io';
 
 
     void main(){
-        
+        print('Informe o valor da compra:');
+        double valor = double.parse( stdin.readLineSync()!);
 
+    
+        if (valor > 100)
+        {
+        double desconto = valor * 0.10;
+        double valorFinal = valor - desconto;
+        print('Você ganhou 10% de desconto:  R\$ $valorFinal');
 
+        }
+
+        else 
+            print('Valor da compra: R\$ $valor');
 
 
     }
+
+
+
